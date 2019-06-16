46309baa9d002121f15872350fd268b0b1b896ef
pavan <pavan1645@gmail.com>
2019-06-16 21:30:49 +0530

    path fix

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const app = express();
 const PORT = 4200;
 
 app.use(compression());
-app.use(express.static(__dirname + "/browser"))
+app.use(express.static(__dirname + "/dist/browser"))
 
 app.get("*", (req, res) => {
-	res.sendFile(__dirname + "/browser/index.html")
+	res.sendFile(__dirname + "/dist/browser/index.html")
 })
 
 app.listen(PORT, () => console.log('Running on PORT', PORT))
